463300c17b2dc8a53dc196ad8bf5eac47289c719
Purushotam <telangpankaj4@gmail.com>
2026-06-16 20:25:25 +0530

    Added Pyramid with spaces

--- Pyramid.java
+++ Pyramid.java
@@ -9,12 +9,29 @@ public class Pyramid{
 
         int n=5;
 
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0; j<i*2+1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        //Centered Pyramid of Stars with space
+        //     * 
+        //    * *
+        //   * * *
+        //  * * * *
+        // * * * * *
+
         for(int i=0; i<n; i++){
             for(int j=0; j<n-i; j++){
                 System.out.print(" ");
             }
-            for(int j=0; j<i*2+1; j++){
-                System.out.print("*");
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
