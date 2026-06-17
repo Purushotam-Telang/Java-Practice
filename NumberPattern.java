12e2c32417e7cf906272e16f4310d1d466fdb262
Purushotam <telangpankaj4@gmail.com>
2026-06-17 15:01:57 +0530

    Added rever number pattern

--- NumberPattern.java
+++ NumberPattern.java
@@ -1,6 +1,6 @@
 public class NumberPattern {
     public static void main(String[] args) {
-        //Number apttern
+        //Number pattern
         //     1
         //    121
         //   12321
@@ -24,6 +24,32 @@ public class NumberPattern {
 
             System.out.println();
         }
+
+        System.out.println("\n\n");
+
+        //Number pattern
+        //     5
+        //    545
+        //   54345
+        //  5432345
+        // 543212345
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            
+            for(int j=1; j <= i; j++){
+                System.out.print(n-j+1+"");
+            }
+
+            for(int j=n-(i-1); j<n; j++){
+                System.out.print((j+1)+"");
+            }
+
+            System.out.println();
+        }
+
     }
     
 }
